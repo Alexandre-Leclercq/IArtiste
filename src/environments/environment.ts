7edb0fb137986f6a1753ea1765aaf8e71adad55f
Alexandre Leclercq <alexleclercq.51@gmail.com>
2023-03-10 10:41:03 +0100

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'iartiste',
+    appId: '1:5564735032:web:b159d6f57ca093e17210cd',
+    storageBucket: 'iartiste.appspot.com',
+    apiKey: 'AIzaSyCJQHRUj2qrdE6e4P7zG_blVFp3sPR-zig',
+    authDomain: 'iartiste.firebaseapp.com',
+    messagingSenderId: '5564735032',
+  },
     production: false
 };
