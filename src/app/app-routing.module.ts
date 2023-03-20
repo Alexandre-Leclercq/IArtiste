8e7dac3f63d11995fbd1cbd12df9d0cec2bc92cd
Adcol13 <ad.colmart@gmail.com>
2023-03-20 11:03:17 +0100

    gallery et clic html ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,16 @@ import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { AuthGuard } from './shared/guard/auth.guard';
+import { GalleryComponent } from './gallery/gallery.component';
+import { GalleryclicComponent } from './galleryclic/galleryclic.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'galleryclic', component: GalleryclicComponent},
+
   {path: 'generate-image', component: RegisterComponent, canActivate: [AuthGuard]},
   {path: 'forgot-password', component: ForgotPasswordComponent},
   {path: 'verify-email-adress', component: VerifyEmailComponent },
